c1ef6befc33997538bd790712d5385c3f10de4ff
rahulnpr <jha6368@gmail.com>
2025-05-17 12:44:48 +0530

    changes

--- src/Components/HomeShop/HomeShop.jsx
+++ src/Components/HomeShop/HomeShop.jsx
@@ -1,78 +1,14 @@
-// import React, { useState } from "react";
-// import styles from "./HomeShop.module.css";
-// import { assets } from '../../assets/assets';
-// import { FaCartPlus } from "react-icons/fa";
-
-// const HomeShop = () => {
-//   const [cart, setCart] = useState([]);
-
-//   // Add sale flag to some products
-//   const productsWithSale = assets.products.map(product => {
-//     // Randomly select some products to have sale (or you can manually specify)
-//     const onSale = [1, 7,8, 9,10, 11,13,14,15,16].includes(product.id); 
-//     return {
-//       ...product,
-//       onSale,
-//       saleText: onSale ? "Sale!" : null
-//     };
-//   });
-
-//   const addToCart = (product) => {
-//     setCart([...cart, product]);
-//     alert(`${product.displayName} added to cart!`);
-//   };
-
-//   return (
-//     <div className={styles.container}>    
-//       <div className={styles.productGrid}>
-//         {productsWithSale.map((product) => (
-//           <div key={product.id} className={styles.productCard}>
-//             <div className={styles.imageContainer}>
-//               {product.onSale && (
-//                 <div className={styles.saleBadge}>{product.saleText}</div>
-//               )}
-//               <img 
-//                 src={product.image} 
-//                 alt={product.name} 
-//                 className={styles.productImage} 
-//               />
-//               <div 
-//                 className={styles.addToCartIcon}
-//                 onClick={() => addToCart(product)}
-//               >
-//                 <FaCartPlus />
-//               </div>
-//             </div>
-//             <div className={styles.productDisplayName}>{product.displayName}</div>
-//             <div className={styles.priceContainer}>
-//               <span className={styles.cutoffPrice}>{product.cutoffPrice}</span>
-//               <span className={styles.productPrice}>{product.price}</span>
-//             </div>
-//             <button 
-//               className={styles.addToCartButton}
-//               onClick={() => addToCart(product)}
-//             >
-//               Add to Cart
-//             </button>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default HomeShop;
 
 
-import React, { useState } from "react";
+import React from "react";
 import styles from "./HomeShop.module.css";
 import { assets } from '../../assets/assets';
 import { FaCartPlus } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
-const HomeShop = () => {
-  const [cart, setCart] = useState([]);
+const HomeShop = ({ addToCart }) => {
+  const navigate = useNavigate();
 
-  // Add sale flag to some products
   const productsWithSale = assets.products.map(product => {
     const onSale = [1, 7, 8, 9, 10, 11, 13, 14, 15, 16].includes(product.id); 
     return {
@@ -82,9 +18,10 @@ const HomeShop = () => {
     };
   });
 
-  const addToCart = (product) => {
-    setCart([...cart, product]);
-    alert(`${product.name} added to cart!`);
+  const handleAddToCart = (product) => {
+    addToCart(product);
+    // remove alert and directly navigate
+    navigate("/cart");
   };
 
   return (
@@ -103,7 +40,7 @@ const HomeShop = () => {
               />
               <div 
                 className={styles.addToCartIcon}
-                onClick={() => addToCart(product)}
+                onClick={() => handleAddToCart(product)}
               >
                 <FaCartPlus />
               </div>
@@ -118,7 +55,7 @@ const HomeShop = () => {
             </div>
             <button 
               className={styles.addToCartButton}
-              onClick={() => addToCart(product)}
+              onClick={() => handleAddToCart(product)}
             >
               Add to Cart
             </button>
---
+++ src/Pages/AddCart/AddCart.jsx
@@ -0,0 +1,30 @@
+import AboutUsBanner from "../../Components/AboutUsBanner/AboutUsBanner";
+import Cart from "../../Components/Cart/Cart";
+import CommonBanner from "../../Components/CommonBanner/CommonBanner";
+import Footer from "../../Components/Footer/Footer";
+import MainNav from "../../Components/MainNav/MainNav";
+import TopNav from "../../Components/TopNav/TopNav";
+import WhyUs from "../../Components/WhyUs/WhyUs";
+
+
+
+
+
+const AddCart = () => {
+  return (
+    <>
+    <TopNav />
+    <MainNav />
+    <Cart />
+    <CommonBanner />
+    <Footer />
+    
+    
+
+   
+     
+    </>
+  );
+};
+
+export default AddCart;
